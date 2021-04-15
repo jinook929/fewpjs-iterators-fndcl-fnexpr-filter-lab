9e71a63b6a2a79abba561a8948c720394b52cfa8
jinook929 <jinook929@gmail.com>
2021-04-15 18:20:43 -0400

    commit tree 3434f737fbff1316f6367cec96160457722d2573

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+function findMatching(names, name) {
+  return names.filter(el => el.toLowerCase() == name.toLowerCase())
+}
+
+function fuzzyMatch(names, name) {
+  return names.filter(el => el.toLowerCase().startsWith(name.toLowerCase()))
+}
+
+function matchName(people, name) {
+  return people.filter(el => el.name.toLowerCase().startsWith(name.toLowerCase()))
+}
+
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+console.log(matchName(drivers, 'Bobby'))
